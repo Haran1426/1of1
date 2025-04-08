346a0eb48dc83c1c2dad5fb1d74dc607875be932
Haran1426 <haningyu08@gmail.com>
2025-04-08 20:54:45 +0900

    GB

---
+++ Assets/Scrips/Damages.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damages : MonoBehaviour
+{
+    [SerializeField]
+    private float Damage;
+
+    [SerializeField]
+    private float Speed;
+    
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    void Tuch()
+    {
+        
+    }
+}
--- Assets/Scrips/Player.cs
+++ Assets/Scrips/Player.cs
@@ -2,19 +2,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    
+    public int HP = 100;
     void Update()
     {
         transform.position = new Vector3(-4.3f, transform.position.y, transform.position.z);
 
-        if (Input.GetKeyDown(KeyCode.R)) // R 키를 누르면 y = 1.5
+        if (Input.GetKeyDown(KeyCode.R)) 
         {
             transform.position = new Vector3(transform.position.x, 1.5f, transform.position.z);
         }
-        else if (Input.GetKeyDown(KeyCode.G)) // G 키를 누르면 y = 0
+        else if (Input.GetKeyDown(KeyCode.G)) 
         {
             transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
         }
-        else if (Input.GetKeyDown(KeyCode.B)) // B 키를 누르면 y = -1.5
+        else if (Input.GetKeyDown(KeyCode.B)) 
         {
             transform.position = new Vector3(transform.position.x, -1.5f, transform.position.z);
         }
