e093e77d794ffd135a7b0410bf24177779e74e36
Haran1426 <haranharan1105@gmail.com>
2025-05-23 09:07:17 +0900

    NoteRecorder

---
+++ Assets/Scrips/NoteRecorder.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteRecorder : MonoBehaviour
+{
+    public AudioSource music;
+    private List<float> noteTimings = new();
+    void Update()
+    {
+        if (music.isPlaying && Input.GetKeyDown(KeyCode.Space))
+        {
+            float t = Time.time;
+            noteTimings.Add(t);
+            Debug.Log($"노트 기록: {t}");
+        }
+        if (Input.GetKeyDown(KeyCode.S)) {
+            string json = JsonUtility.ToJson(new Wrapper { timings = noteTimings.ToArray() });
+            System.IO.File.WriteAllText(Application.dataPath + "/noteData.json", json);
+            Debug.Log("저장됨");
+
+        }
+    }
+
+    [System.Serializable]
+    public class Wrapper { public float[] timings;}
+}
