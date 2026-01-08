2300fb1258975e422db8e7b54faede66001b2d9e
Haningyu <haranharan1105@gmail.com>
2026-01-08 23:53:50 +0900

    2

--- Assets/Scrips/Note/Note.cs
+++ Assets/Scrips/Note/Note.cs
@@ -1,6 +1,24 @@
 ﻿using System.Collections;
 using UnityEngine;
+using System;
+using System.Collections.Generic;
+[Serializable]
+public class NoteData
+{
+    public int type;       // 1=Red, 2=Green, ...
+    public float posX;
+    public float posY;
+    public float distance; // 이전 노트와 거리
+    public float beat;     // 이전 노트 대비 박자 차이
+}
 
+[Serializable]
+public class MapData
+{
+    public string songName;
+    public float bpm;
+    public List<NoteData> notes;
+}
 public class Note : MonoBehaviour
 {
     public NoteSpawner.NoteType noteType;
